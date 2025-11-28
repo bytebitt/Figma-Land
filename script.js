ebc5527141e15f9fe9a63cb3ef9cdb60d0778169
bytebitt <Vetal201710@gmail.com>
2025-11-28 11:16:04 +0200

    added bureger menu

---
+++ script.js
@@ -0,0 +1,12 @@
+const menu = document.getElementById("mobileMenu");
+const btn = document.getElementById("burgerBtn");
+const exit = document.getElementById("exit");
+("exit");
+
+btn.addEventListener("click", () => {
+  menu.style.left = menu.style.left === "0px" ? "-100%" : "0";
+});
+
+exit.addEventListener("click", () => {
+  menu.style.left = "-100%";
+});
